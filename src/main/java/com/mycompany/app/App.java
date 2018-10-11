d1f9d4a43003e4819d790a67dd69b08ada5696a0
Miguel Borja <miguel.borja@i2btech.com>
2018-10-11 12:57:03 -0500

    Agregado Sleep

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,15 +3,17 @@ package com.mycompany.app;
 /**
  * Hello world!
  */
-public class App
-{
+public class App {
 
     private final String message = "Hello World!";
 
-    public App() {}
+    public App() {
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println(new App().getMessage());
+        Thread.sleep(120000);
+        System.out.println("Terminado...");
     }
 
     private final String getMessage() {
